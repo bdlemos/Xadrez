2d0883dc546f7c2c82be7c17d0364d3a98df6660
Bernardo <bdutralemos@gmail.com>
2022-07-18 13:34:12 -0300

    Colocando pecas no tabuleiro - inicio

---
+++ xadrez/Torre.cs
@@ -0,0 +1,11 @@
+using tabuleiro;
+namespace xadrez{
+    class Torre : Peca{
+        public Torre(Tabuleiro tab, Cor cor) : base(tab,cor){
+        }
+        public override string ToString(){
+            return "T";
+        }
+        
+    }
+}
